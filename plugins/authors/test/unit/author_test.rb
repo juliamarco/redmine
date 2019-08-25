e799d028689436e4c91b33a5bbb85920bf6ae540
Julia Marco <marcootj@gmail.com>
2019-08-25 14:34:43 +0100

    Creates Author model

---
+++ plugins/authors/test/unit/author_test.rb
@@ -0,0 +1,14 @@
+require File.expand_path('../../test_helper', __FILE__)
+require 'shoulda/matchers'
+
+
+
+
+class AuthorTest < ActiveSupport::TestCase
+
+  # Replace this with your real tests.
+  def assert_relationship
+
+  end
+
+end
